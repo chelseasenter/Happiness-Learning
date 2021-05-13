70b0b75cadcf5fb6a2ae00f7fb7eede8192a288a
Evan Mobley <emmobley63@gmail.com>
2021-05-13 10:46:23 -0400

    ,Added plotly.react but I need the HTML to start testing and create the graph

--- Webpage/scripts/create.js
+++ Webpage/scripts/create.js
@@ -7,6 +7,7 @@ var allowance = d3.select("#allowancebox").attr("value")
 
 var xarray = ['economy_gdp_per_capita', 'health_life_expectancy', 'freedom', 'trust_government_corruption', 'generosity'];
 
+var yarray = [0, 0, 0, 0, 0]
 // Creating an even on the click of the plus or minus sign //
 d3.selectAll(#plussign).on("click", CategoryLevel.addition);
 
@@ -34,12 +35,20 @@ class CategoryLevel{
 var data = [
     {
       x: xarray,
-      y: [0, 0, 0, 0, 0],
+      y: yarray,
       type: 'bar'
     }
   ];
   
-  Plotly.newPlot('myBar', data);
+  Plotly.newPlot('myBar', data, {responsive: true});
 
   // Update Plotly to update the score for each bar when adding or subtracting score //
-  
+
+  class UpdatePlotly{
+    data = {
+        x: xarray,
+        y: yarray,
+        type: 'bar'
+      }  
+    Plotly.react('myBar', data)
+  }
