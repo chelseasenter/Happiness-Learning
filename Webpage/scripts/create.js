81efa2e7a5bdf282008df17d9c3526b099200917
Evan Mobley <emmobley63@gmail.com>
2021-05-13 10:14:04 -0400

    ,Added arrrays, d3 for minus and plus, and a basic bar chart

--- Webpage/scripts/create.js
+++ Webpage/scripts/create.js
@@ -2,17 +2,22 @@ var url = "http://127.0.0.1:5000/";
 
 querlUrl = url + "insert the correct query"
 
+// Establishing allowance as a variable that can be changed //
 var allowance = d3.select("#allowancebox").attr("value")
-d3.select(#plusSign).on("click");
 
-updateScore {
-    firstbox = d3.select(#firstbox).
-}
+var xarray = ['economy_gdp_per_capita', 'health_life_expectancy', 'freedom', 'trust_government_corruption', 'generosity'];
+
+// Creating an even on the click of the plus or minus sign //
+d3.selectAll(#plussign).on("click", CategoryLevel.addition);
+
+d3.selectAll(#minussign).on("click", CategoryLevel.subtraction);
+
 
 class CategoryLevel{
     constructor(category){
         this.category = category;
         this.score = 0;
+        //might need to pass in a index here for the graph//
     }
 
     addition(amount){
@@ -21,8 +26,20 @@ class CategoryLevel{
     }
 
     subtraction(amount){
-        this.score -= 1;
+        this.score -= amount;
         allowance += allowance
     }
-}
+};
 
+var data = [
+    {
+      x: xarray,
+      y: [0, 0, 0, 0, 0],
+      type: 'bar'
+    }
+  ];
+  
+  Plotly.newPlot('myBar', data);
+
+  // Update Plotly to update the score for each bar when adding or subtracting score //
+  
